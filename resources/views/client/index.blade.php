f4874358985839c06789cbeb59b118bc404b6805
chhatraman shrestha <cms111000111@gmail.com>
2024-05-25 12:19:55 +0545

    index updated

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -1,4 +1,24 @@
 @extends('client.layout.app')
 @section('content')
-
+    <div class="dashboard">
+        <div class="mt-2 mx-0 mx-md-4 mt-md-4">
+            <div class="row">
+                <div class="col-md-4 ">
+                    <a  href="{{route('client.submission.index')}}">
+                        My Submissions
+                    </a>
+                </div>
+                <div class="col-md-4  ">
+                    <a  href="{{route('client.info.index')}}">
+                        My Profile
+                    </a>
+                </div>
+                <div class="col-md-4 ">
+                    <a  href="{{route('client.info.password')}}">
+                        Change Password
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection

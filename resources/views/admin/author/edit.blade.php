a35e63d608aa7d82a12c82d7b0dbdf4bc6c41904
chhatraman shrestha <cms111000111@gmail.com>
2024-05-27 13:22:40 +0545

    author edit changed

--- resources/views/admin/author/edit.blade.php
+++ resources/views/admin/author/edit.blade.php
@@ -9,19 +9,19 @@
         <form action="{{ route('admin.author.edit',['author_id'=>$author->id]) }}" method="POST">
             @csrf
             <div class="row align-items-end">
-                <div class="col-md-4 mb-2">
+                <div class="col-md-6 mb-2">
                     <label for="name">Name</label>
                     <input type="text" name="name" id="name" value="{{$author->name}}" class="form-control" required>
                 </div>
-                <div class="col-md-4 mb-2">
+                <div class="col-md-6 mb-2">
                     <label for="link">Link</label>
                     <input type="text" name="link" id="link" value="{{$author->link}}" class="form-control" >
                 </div>
-                <div class="col-md-4 mb-2">
+                <div class="col-md-12 mb-2">
                     <label for="designation">Designation</label>
                     <input type="text" name="designation" id="designation" value="{{$author->designation}}" class="form-control" >
                 </div>
-                <div class="col-md-8 mb-2">
+                <div class="col-md-12 mb-2">
                     <label for="organization">Organization</label>
                     <input type="text" name="organization" id="organization" value="{{$author->organization}}" class="form-control" >
                 </div>

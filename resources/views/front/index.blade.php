f7b49188bd74f4ebbf2e3348c5a36b15dbbc064e
chhatraman shrestha <cms111000111@gmail.com>
2025-09-04 10:47:50 +0545

    Refactor index view to conditionally include cached home view and display a message when no publications are available

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -4,5 +4,18 @@
 
 @endsection
 @section('content')
-    @includeIf('front.cache.home')
+    @if(View::exists('front.cache.home'))
+        @include('front.cache.home')
+    @else
+        <div class="text-center py-5">
+            <!-- Simple document/launch SVG icon -->
+            <svg width="96" height="96" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
+                <rect x="4" y="3" width="16" height="18" rx="2" fill="#E2E8F0" />
+                <path d="M8 7h8v2H8zM8 11h8v2H8zM8 15h5v2H8z" fill="#1A202C" />
+                <path d="M12 2l2.5 4H9.5L12 2z" fill="#4A5568" />
+            </svg>
+            <h2 class="mt-4">We don't have a current publication</h2>
+            <p>We will be releasing soon.</p>
+        </div>
+    @endif
 @endsection
